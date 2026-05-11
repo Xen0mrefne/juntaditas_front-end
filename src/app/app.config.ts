8974efe2258ac22b12fbdf68dc60efa281e1b8dd
Xen0mrefne <x3n0g3n1c@gmail.com>
2026-05-10 23:00:15 -0300

    updated angular to v18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { interceptorProvider } from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
